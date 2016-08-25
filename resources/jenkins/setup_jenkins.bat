46d87540c55ae687e517c07bbc2feb7a4c527f14
wchen-r7 <wei_chen@rapid7.com>
2016-08-25 14:38:59 -0500

    add np flag to schtasks for Jenkins

--- resources/jenkins/setup_jenkins.bat
+++ resources/jenkins/setup_jenkins.bat
@@ -1,5 +1,5 @@
 mkdir "%ProgramFiles%\jenkins"
 copy C:\vagrant\resources\jenkins\jenkins.war "%ProgramFiles%\jenkins"
 copy C:\vagrant\resources\jenkins\start_jenkins.bat "%ProgramFiles%\jenkins"
-schtasks /create /tn "Jenkins" /tr "\"%ProgramFiles%\jenkins\start_jenkins.bat\"" /sc onstart
+schtasks /create /tn "Jenkins" /tr "\"%ProgramFiles%\jenkins\start_jenkins.bat\"" /sc onstart /np
 schtasks /run /tn "Jenkins"
