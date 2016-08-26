795d8606ed316186883ae7167a9b67cfc9a3a1dc
James Barnett <James_Barnett@rapid7.com>
2016-08-26 16:48:39 -0500

    Change Tomcat to listen on 8282 since GlassFish needs 8080.

--- resources/glassfish/setup_glassfish.bat
+++ resources/glassfish/setup_glassfish.bat
@@ -1,6 +1,5 @@
 mkdir C:\glassfish
 xcopy /s /e C:\vagrant\resources\glassfish\glassfish4_2 C:\glassfish
 
-REM copy C:\vagrant\resources\glassfish\start_glassfish.bat C:\glassfish\
 schtasks /create /tn "GlassFish" /tr "C:\glassfish\bin\asadmin.bat start-domain domain1" /sc onstart /np
 schtasks /run /tn "GlassFish"
